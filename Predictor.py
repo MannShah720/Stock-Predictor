2f6b2ef45cdc7c64ab91ff72829cab887c4f809a
Mann Shah <shahmann0108@gmail.com>
2026-08-30 22:12:52 +0100

    added csv file and reader

--- Predictor.py
+++ Predictor.py
@@ -2,3 +2,15 @@ import csv
 import numpy as np
 import sklearn.svm as SVR
 import matplotlib.pyplot as plt
+
+dates = []
+prices = []
+
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)  # Skip the header
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+        return
